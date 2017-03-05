b2216d9999e350f430e57b7e5b83ada68057fe20
Sinclair D <sinclairdong@gmail.com>
2017-03-04 22:52:16 -0500

    finished clock.c

--- clock.c
+++ clock.c
@@ -21,16 +21,30 @@ int clock_evict() {
 	pgtbl_entry_t *current;
 	
 	//clock starts at frame 0
-	//i know i don't have to cuz it will be 0 anyway but that is good styleing
-	i = 0;
+
 	
 	// let's use an int since we don't have boolean in C
 	// I Know... C sucks.  :(
 	int exit = 1;
 	while(exit){
 	    current = coremap[i].pte;
+	    if((current->grame & PG_REF) == 0){
+	        // return the index
+	        exit = 0;
+	    }else{
+	        // now set it to not used since it has been checked
+	        current->grame &= ~PG_REF;
+	        i++;
+	    }
+	    
+	    // need to move to start of coremap if next iteration is >= memsize
+		// because we may have starting somewhere in the middle of coremap
+		// and because we want to loop "circularly" until we find an unused PTE
+		if(i + 1 >= memsize) {
+			i = 0;
+		}
 	}
-	return 0;
+	return i;
 }
 
 /* This function is called on each access to a page to update any information
@@ -38,7 +52,7 @@ int clock_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void clock_ref(pgtbl_entry_t *p) {
-
+	p->frame |= PG_REF;
 	return;
 }
 
@@ -46,4 +60,6 @@ void clock_ref(pgtbl_entry_t *p) {
  * algorithm. 
  */
 void clock_init() {
+    //i know i don't have to cuz it will be 0 anyway but that is good styleing
+	i = 0;
 }
